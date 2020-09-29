ba24bf5c6e3e5e90929fafd6353e6c798ed74ee4
Anton <you@example.com>
2020-09-29 16:32:17 +0300

    comment for show_progress function

--- diets/views.py
+++ diets/views.py
@@ -8,6 +8,7 @@ from django.contrib import messages
 from .diets_services import *
 
 
+# Function shows for user his history of weight and graph
 @login_required()
 def show_progress(request):
     user = request.user
